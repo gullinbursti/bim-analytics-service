c29c08363598a15a7b1a1e4a20f60f1f71be0adb
Pedro <digitalrounin@gmail.com>
2014-10-06 12:07:45 +0700

    Adding Django config information to tox

---
+++ tests-config/local_settings.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+# ^^^ The above is required if you want to import from the celery
+# library.  If you don't have this then `from celery.schedules import`
+# becomes `proj.celery.schedules` in Python 2.x since it allows
+# for relative imports by default.
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+# -----------------------------------------------------------------------------
+# Celery settings
+BROKER_URL = None
+
+# -----------------------------------------------------------------------------
+# Django
+SECRET_KEY = 'SECRET_KEY_ERROR_NEVER_USE_IN_PRODUCTION'
