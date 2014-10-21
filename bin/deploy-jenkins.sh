d5953f2b613054e89c67527fe4fc7e89a5b789eb
Pedro H <pedro@builtinmenlo.com>
2014-10-21 09:38:25 +0000

    Remote install process

--- bin/deploy-jenkins.sh
+++ bin/deploy-jenkins.sh
@@ -19,19 +19,24 @@ package_path=$(ls -1 $dist_dir/bimanalytics-*.tar.gz | sort -u | tail -1)
 
 ssh_proxy='ProxyCommand ssh bim-deploy@bastion.selfieclubapp.com nc -q0 %h %p 2> /dev/null'
 remote_host='bim-deploy@api00.devint.selfieclubapp.com'
-remote_tmp_dir="/opt/built-in-menlo/tmp"
+remote_tmp_dir="/opt/built-in-menlo/tmp/bimanalytics/$build_id"
 
 echo ""
 echo "-------------------------------------------------------------------------------"
 echo "- *LOCAL* Shipping: $build_id"
 echo "-------------------------------------------------------------------------------"
-scp -o "$ssh_proxy" "$package_path" "bin/deploy-remote.sh" "$remote_host:."
 ssh -o "$ssh_proxy" "$remote_host" <<EOC
     set -o xtrace
     set -o errexit
-    which mkdir
     test ! -d "$remote_tmp_dir" || exit 1
     mkdir -pv "$remote_tmp_dir"
+EOC
+scp -o "$ssh_proxy" "$package_path" "bin/deploy-remote.sh" "$remote_host:$remote_tmp_dir/"
+ssh -o "$ssh_proxy" "$remote_host" <<EOC
+    set -o xtrace
+    set -o errexit
+    cd "$remote_tmp_dir/ksjdfhksdjfh"
+    echo BADDDD!!!!
 EOC
 
 
