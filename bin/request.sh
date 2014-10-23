35572b454d9f7091ac71b072b92f0d6752ea7886
Pedro H <pedro@builtinmenlo.com>
2014-10-23 18:12:09 +0700

    Sample curl command with JSON.

--- bin/request.sh
+++ bin/request.sh
@@ -3,6 +3,6 @@
 curl -v --request POST \
     -H "Content-Type: application/json" \
     -H 'Accept: application/json; indent=4' \
-    "http://127.0.0.1:8000/selfieclub/" \
+    "http://stats.devint.selfieclubapp.com/selfieclub/" \
     --data '@request.json'
 
