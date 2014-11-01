71c46bb7078bcdd6e643b84357d371963797780f
Pedro H <pedro@builtinmenlo.com>
2014-11-01 10:30:20 +0700

    Clean up and setting up to talk to Keen

--- src/selfieclub/tasks.py
+++ src/selfieclub/tasks.py
@@ -3,19 +3,24 @@
 from __future__ import absolute_import
 from celery.utils.log import get_task_logger
 from celery import shared_task
+from selfieclub.serializers import AnalyticsEventSerializer
 
 
 LOGGER = get_task_logger(__name__)
 
 
-@shared_task
+# @shared_task(bind=True, ignore_result=True, max_retries=5, )  # TODO - add
+# self
+@shared_task(ignore_result=True, max_retries=6,
+             default_retry_delay=60*5)  # 6 reties, 5 min appart.
 def record_event(event):
+    # pylint: disable=no-member, no-value-for-parameter, unexpected-keyword-arg
     """Foo the bar."""
-    LOGGER.debug('This is from the celery task \'debug\'...')
-    LOGGER.info('This is from the celery task \'info\'...')
-    LOGGER.warning('This is from the celery task \'warning\'...')
-    LOGGER.error('This is from the celery task \'error\'...')
-    LOGGER.critical('This is from the celery task \'critical\'...')
+    serializer = AnalyticsEventSerializer(data=event)
+    if not serializer.is_valid():
+        LOGGER.warning('Invalid event.')
+        LOGGER.debug('Invalid event details: %s', event)
+
     LOGGER.info('Event to record: %s', event)
 
 
--- src/selfieclub/views.py
+++ src/selfieclub/views.py
@@ -18,19 +18,18 @@ class EventView(APIView):
 
     # pylint: disable=too-many-public-methods
     def post(self, request, format=None):  # pylint: disable=redefined-builtin
-        # noqa pylint: disable=unexpected-keyword-arg, no-value-for-parameter, no-member
+        # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
+        # pylint: disable=no-member
         """Process incoming event that has been POSTed."""
-        request_serializer = AnalyticsEventSerializer(data=request.DATA)
-        if not request_serializer.is_valid():
-            return Response(request_serializer.errors,
+        serializer = AnalyticsEventSerializer(data=request.DATA)
+        if not serializer.is_valid():
+            LOGGER.warning('Invalid message.')
+            LOGGER.debug('Invalid message details: %s', request.DATA)
+            return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
-        LOGGER.debug('This is from the django view \'debug\'...')
-        LOGGER.info('This is from the django view \'info\'...')
-        LOGGER.warning('This is from the django view \'warning\'...')
-        LOGGER.error('This is from the django view \'error\'...')
-        LOGGER.critical('This is from the django view \'critical\'...')
-        LOGGER.info('Sending: %s', request_serializer)
-        tasks.record_event.delay(request_serializer.data)
 
-        rest_response = Response(None, status=status.HTTP_200_OK)
-        return rest_response
+        # TODO - Message GUID
+        LOGGER.info('Sending: TODO-GUID')
+        LOGGER.debug('Sending message body: %s', serializer.data)
+        tasks.record_event.delay(serializer.data)
+        return Response(None, status=status.HTTP_200_OK)
