802cb8ac4c53cb3625ed6c8204419166ad1ffe8e
Pedro H <pedro@builtinmenlo.com>
2014-11-02 11:30:29 +0700

    Unit tests added for tasks.

--- src/selfieclub/tasks.py
+++ src/selfieclub/tasks.py
@@ -5,6 +5,7 @@ from celery.utils.log import get_task_logger
 from celery import shared_task
 from django.conf import settings
 from selfieclub.serializers import AnalyticsEventSerializer
+from keen.exceptions import KeenApiError
 import keen
 
 
@@ -15,7 +16,7 @@ LOGGER = get_task_logger(__name__)
              default_retry_delay=60*5)  # 6 reties, 5 min appart.
 def record_event(self, event):
     # pylint: disable=no-member, no-value-for-parameter, unexpected-keyword-arg
-    """Foo the bar."""
+    """Send client analitics event to Keen IO."""
     serializer = AnalyticsEventSerializer(data=event)
     if not serializer.is_valid():
         # Note that if serialization fails, serializer.data is bad, use
@@ -31,7 +32,7 @@ def record_event(self, event):
         LOGGER.debug('Event to record details: %s', serializer.data)
         keen.add_event(settings.KEEN_IO_EVENT_COLLECTIONS['client_event'],
                        serializer.data)
-    except keen.exceptions.KeenApiError as exception:
+    except KeenApiError as exception:
         LOGGER.exception('Failed to record event to upstream.',
                          exc_info=exception)
         raise self.retry(exc=exception)
---
+++ tests/selfieclub/test_tasks.py
@@ -0,0 +1,62 @@
+"""Test BIM Analytics selfieclub tasks."""
+
+from __future__ import absolute_import
+from keen.exceptions import KeenApiError
+from mock import patch
+from selfieclub import tasks
+import selfieclub
+
+
+@patch('selfieclub.tasks.settings.CELERY_ALWAYS_EAGER', True, create=True)
+def test_record_event_successful_send():
+    """Test that event is send to Keen when all i s good."""
+    with patch('selfieclub.tasks.AnalyticsEventSerializer') as clazz, \
+            patch('selfieclub.tasks.keen') as keen:
+        # Arrange
+        serializer = clazz.return_value
+        serializer.is_valid.return_value = True
+        # Act
+        # TODO - Pass in an actual dict
+        tasks.record_event.delay({'faking': 'it'})
+        # Assert
+        serializer.is_valid.assert_called()
+        keen.add_event.assert_called_with('Basic Client Event - DEVINT',
+                                          serializer.data)
+
+
+@patch('selfieclub.tasks.settings.CELERY_ALWAYS_EAGER', True, create=True)
+def test_record_event_invalid_data_does_not_send():
+    """Test that invalid data is dropped, and not sent to Keen."""
+    with patch('selfieclub.tasks.AnalyticsEventSerializer') as clazz, \
+            patch('selfieclub.tasks.keen') as keen:
+        # Arrange
+        serializer = clazz.return_value
+        serializer.is_valid.return_value = False
+        # Act
+        # TODO - Pass in an actual dict
+        tasks.record_event.delay({'faking': 'it'})
+        # Assert
+        serializer.is_valid.assert_called()
+        assert not keen.add_event.mock_calls
+
+
+@patch('selfieclub.tasks.settings.CELERY_ALWAYS_EAGER', True, create=True)
+def test_record_event_rety_on_keen_error():
+    """Test that an even is requeued for retying on Keen exceptions."""
+    with patch('selfieclub.tasks.AnalyticsEventSerializer') as clazz, \
+            patch('selfieclub.tasks.keen') as keen, \
+            patch.object(selfieclub.tasks.record_event, 'retry') as retry:
+        # Arrange
+        serializer = clazz.return_value
+        serializer.is_valid.return_value = True
+        exception = KeenApiError({
+            'message': 'mock message',
+            'error_code': 500})
+        keen.add_event.side_effect = exception
+        retry.return_value = exception
+        # Act
+        # TODO - Pass in an actual dict
+        tasks.record_event.delay({'faking': 'it'})
+        # Assert
+        keen.add_event.assert_called()
+        retry.assert_called_with(exc=exception)
